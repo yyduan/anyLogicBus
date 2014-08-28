8bf633e310e3f235ce2f5c9e42d85839d4536df1
duanyy <hmyyduan@gmail.com>
2014-08-28 11:48:13 +0800

    1.2.6.5
    LogicBusApp:增加onInit/onDestroy事件，以便子类更好的进行初始化和销毁

--- README.md
+++ README.md
@@ -113,3 +113,4 @@ anyLogicBus是一个高效的,可扩展的,轻量级服务框架,具有下列特
      + 优化ACM访问控制模型的缺省模型存储机制(1.2.6.2)
      + 配合基础类库Pool的修改(1.2.6.3)
      + MessageRouter:修正servant实例无法获取到，抛出NullPointException问题(1.2.6.4)
+     + LogicBusApp:增加onInit/onDestroy事件，以便子类更好的进行初始化和销毁(1.2.6.5)
--- src/com/logicbus/backend/server/LogicBusApp.java
+++ src/com/logicbus/backend/server/LogicBusApp.java
@@ -10,7 +10,6 @@ import org.w3c.dom.Document;
 
 import com.anysoft.util.DefaultProperties;
 import com.anysoft.util.IOTools;
-import com.anysoft.util.Properties;
 import com.anysoft.util.PropertiesConstants;
 import com.anysoft.util.Settings;
 import com.anysoft.util.XmlTools;
@@ -39,6 +38,10 @@ import com.logicbus.backend.timer.TimerManager;
  * 
  * @version 1.2.6 [20140807 duanyy] <br>
  * - ServantPool和ServantFactory插件化
+ * 
+ * @version 1.2.6.5 [20140828 duanyy] <br>
+ * - 增加onInit/onDestroy事件，以便子类更好的进行初始化和销毁. <br>
+ * 
  */
 public class LogicBusApp implements WebApp {
 	/**
@@ -46,97 +49,11 @@ public class LogicBusApp implements WebApp {
 	 */
 	protected static Logger logger = LogManager.getLogger(LogicBusApp.class);
 		
-	/**
-	 * 启动定时器
-	 * @param settings 参数
-	 * @param resourceFactory 资源工厂
-	 * @param classLoader
-	 */
-	private void startTimer(Properties settings,
-			ResourceFactory resourceFactory,ClassLoader classLoader) {
-		String __tmClass = settings.GetValue("timer.manager",
-				"com.logicbus.backend.timer.TimerManager");
+	protected void onInit(Settings settings){	
+		ClassLoader classLoader = Settings.getClassLoader();
+		ResourceFactory resourceFactory = Settings.getResourceFactory();
 		
-		String __timerConfig = settings.GetValue("timer.config.master", 
-				"java:///com/logicbus/backend/timer/timer.xml#com.logicbus.backend.server.LogicBusApp");
-		if (__timerConfig == null || __timerConfig.length() <= 0)
-			return;
-
-		String __timerSecondaryConfig = settings.GetValue("timer.config.secondary", 
-				"java:///com/logicbus/backend/timer/timer.xml#com.logicbus.backend.server.LogicBusApp");
-		
-		logger.info("Start timer..");
-
-		ResourceFactory rm = new ResourceFactory();
-		InputStream in = null;
-		try {
-			in = rm.load(__timerConfig,__timerSecondaryConfig, null);
-			Document doc = XmlTools.loadFromInputStream(in);
-			// 启动定时器
-			TimerManager __tm = TimerManager.get(__tmClass,classLoader);
-			__tm.schedule(doc.getDocumentElement());
-
-			logger.info("Start timer..OK!");
-		} catch (Exception ex) {
-			logger.error("Error loading xml file,source=" + __timerConfig, ex);
-			logger.info("Start timer..Failed!");
-		} finally {
-			IOTools.closeStream(in);
-		}
-	}
-
-	@Override
-	public void init(DefaultProperties props,ServletContext sc) {
-		Settings settings = Settings.get();
-		settings.addSettings(props);
-		
-		// 初始化一些object		
-		ClassLoader classLoader = (ClassLoader) sc.getAttribute("classLoader");
-		if (classLoader == null){
-			classLoader = LogicBusApp.class.getClassLoader();
-		}
-		settings.registerObject("classLoader", classLoader);
-		
-		//resourceFactory
-		String rf = settings.GetValue("resource.factory","com.anysoft.util.resource.ResourceFactory");
-		ResourceFactory resourceFactory = null;
-		try {
-			logger.info("Use resource factory:" + rf);
-			resourceFactory = (ResourceFactory) classLoader.loadClass(rf).newInstance();
-		} catch (InstantiationException | IllegalAccessException
-				| ClassNotFoundException e) {
-			logger.error("Can not create instance of :" + rf);
-		}
-		if (resourceFactory == null){
-			resourceFactory = new ResourceFactory();
-			logger.info("Use default:" + ResourceFactory.class.getName());
-		}
-		settings.registerObject("ResourceFactory", resourceFactory);
-		
-		//先装入扩展的配置文件
-		String extProfile = settings.GetValue("settings.ext.master", "");
-		String extSecondaryProfile = settings.GetValue("settings.ext.secondary", "");
-		if (extProfile != null && extProfile.length() > 0 
-				&& extSecondaryProfile != null && extSecondaryProfile.length() > 0){
-			logger.info("Load ext xml settings");
-			logger.info("Url = " + extProfile);
-			settings.addSettings(extProfile,extSecondaryProfile,resourceFactory);
-			logger.info("Load xml settings..OK!");
-		}
-		
-		// 装入配置文件
-		String profile = settings.GetValue("settings.master",
-				"java:///com/logicbus/backend/server/http/profile.xml#com.logicbus.backend.server.LogicBusApp");	
-		String secondary_profile = settings.GetValue("settings.secondary",
-				"java:///com/logicbus/backend/server/http/profile.xml#com.logicbus.backend.server.LogicBusApp");
-		
-		logger.info("Load xml settings..");
-		logger.info("Url = " + profile);
-		settings.addSettings(profile,secondary_profile,resourceFactory);
-		logger.info("Load xml settings..OK!");
-
-		String encoding = settings.GetValue("http.encoding","utf-8");
-		XmlTools.setDefaultEncoding(encoding);
+		XmlTools.setDefaultEncoding(settings.GetValue("http.encoding","utf-8"));
 		
 		//初始化AccessController
 		{
@@ -205,16 +122,93 @@ public class LogicBusApp implements WebApp {
 			settings.registerObject("servantFactory", sf);
 		}
 		// 启动定时器
-		startTimer(settings, resourceFactory,classLoader);
+		{
+			String __tmClass = settings.GetValue("timer.manager",
+					"com.logicbus.backend.timer.TimerManager");
+			
+			String __timerConfig = settings.GetValue("timer.config.master", 
+					"java:///com/logicbus/backend/timer/timer.xml#com.logicbus.backend.server.LogicBusApp");
+
+			String __timerSecondaryConfig = settings.GetValue("timer.config.secondary", 
+					"java:///com/logicbus/backend/timer/timer.xml#com.logicbus.backend.server.LogicBusApp");
+			if (__timerConfig != null && __timerConfig.length() > 0){
+				logger.info("Start timer..");
+				InputStream in = null;
+				try {
+					in = resourceFactory.load(__timerConfig,__timerSecondaryConfig, null);
+					Document doc = XmlTools.loadFromInputStream(in);
+					// 启动定时器
+					TimerManager __tm = TimerManager.get(__tmClass,classLoader);
+					__tm.schedule(doc.getDocumentElement());
+
+					logger.info("Start timer..OK!");
+				} catch (Exception ex) {
+					logger.error("Error loading xml file,source=" + __timerConfig, ex);
+					logger.info("Start timer..Failed!");
+				} finally {
+					IOTools.closeStream(in);
+				}
+			}		
+		}
+	}
+	
+	@Override
+	public void init(DefaultProperties props,ServletContext sc) {
+		Settings settings = Settings.get();
+		settings.addSettings(props);
+		
+		// 初始化一些object		
+		ClassLoader classLoader = (ClassLoader) sc.getAttribute("classLoader");
+		if (classLoader == null){
+			classLoader = LogicBusApp.class.getClassLoader();
+		}
+		settings.registerObject("classLoader", classLoader);
+		
+		//resourceFactory
+		String rf = settings.GetValue("resource.factory","com.anysoft.util.resource.ResourceFactory");
+		ResourceFactory resourceFactory = null;
+		try {
+			logger.info("Use resource factory:" + rf);
+			resourceFactory = (ResourceFactory) classLoader.loadClass(rf).newInstance();
+		} catch (InstantiationException | IllegalAccessException
+				| ClassNotFoundException e) {
+			logger.error("Can not create instance of :" + rf);
+		}
+		if (resourceFactory == null){
+			resourceFactory = new ResourceFactory();
+			logger.info("Use default:" + ResourceFactory.class.getName());
+		}
+		settings.registerObject("ResourceFactory", resourceFactory);
+		
+		//先装入扩展的配置文件
+		String extProfile = settings.GetValue("settings.ext.master", "");
+		String extSecondaryProfile = settings.GetValue("settings.ext.secondary", "");
+		if (extProfile != null && extProfile.length() > 0 
+				&& extSecondaryProfile != null && extSecondaryProfile.length() > 0){
+			logger.info("Load ext xml settings");
+			logger.info("Url = " + extProfile);
+			settings.addSettings(extProfile,extSecondaryProfile,resourceFactory);
+			logger.info("Load xml settings..OK!");
+		}
+		
+		// 装入配置文件
+		String profile = settings.GetValue("settings.master",
+				"java:///com/logicbus/backend/server/http/profile.xml#com.logicbus.backend.server.LogicBusApp");	
+		String secondary_profile = settings.GetValue("settings.secondary",
+				"java:///com/logicbus/backend/server/http/profile.xml#com.logicbus.backend.server.LogicBusApp");
+		
+		logger.info("Load xml settings..");
+		logger.info("Url = " + profile);
+		settings.addSettings(profile,secondary_profile,resourceFactory);
+		logger.info("Load xml settings..OK!");
+
+		onInit(settings);
 	}
 
-	@Override
-	public void destroy(ServletContext sc) {
+	protected void onDestroy(Settings settings){
 		TimerManager __tm = TimerManager.get();
 		logger.info("Stop timer..");
 		__tm.stop();
-		
-		Settings settings = Settings.get();
 		
 		ServantFactory sf = (ServantFactory)settings.get("servantFactory");
 		if (sf != null){
@@ -235,5 +229,9 @@ public class LogicBusApp implements WebApp {
 				
 			}
 		}
+	}
+	@Override
+	public void destroy(ServletContext sc) {
+		onDestroy(Settings.get());
 	}
 }
